4a27640204bca10d364c5f2e11a5f61fc6aec987
Luis Alberto Cortez Gomez <alberto.cortez@uicui.edu.mx>
2020-09-19 22:46:01 -0500

    Programa 1.0.1 con gtk

--- main.cpp
+++ main.cpp
@@ -2,18 +2,25 @@
 #include <mysql.h>
 #include <mysqld_error.h>
 #include "generar_lista_mensual.h"
+#include <gtk/gtk.h>
 using namespace std;
-int main(int argc, char* argv[]) {
-	/*VARIABLES*/
-	/*COMIENZA*/
-	struct listaCalMensual a;
-	a = generar_lista_mensual("AECJ940429HCHRRS01", "08");
-	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < 3; j++) {
-			cout<<a.rs[i][j];
-		}
-		cout << endl;
-	}
-	return 0;
-	/*TERMINA*/
+
+static void activate(GtkApplication* app, gpointer user_data){
+    GtkWidget* window;
+    window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Window");
+    gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+    gtk_widget_show_all(window);
+}
+
+int main(int argc, char** argv){
+    GtkApplication* app;
+    int status;
+
+    app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+
+    return status;
 }
